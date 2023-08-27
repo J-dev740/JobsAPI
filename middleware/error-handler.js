1e55decc55ea306319a156eacd3374f3d5b9d430
J-dev740 <tcr19ec029@gectcr.ac.in>
2023-08-27 10:00:39 +0530

    updated to user friendly errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,21 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+
+  let customError={
+    statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg:err.message || 'something went wrong try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if(err.code && err.code===11000){
+    customError.msg=`duplicate value for  ${Object.keys(err.keyValue)} please choose another valid value`
+    customError.statusCode=400
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg:customError.msg})
+
 }
 
 module.exports = errorHandlerMiddleware
