54c822e58ecc47931199008439169bbc222869d0
J-dev740 <tcr19ec029@gectcr.ac.in>
2023-08-29 22:51:57 +0530

    ready for production deployment

--- app.js
+++ app.js
@@ -2,13 +2,33 @@ require('dotenv').config();
 require('express-async-errors');
 const authenticateUser=require('./middleware/authentication')
 const express = require('express');
+//for extra security 
+const helmet = require ('helmet')
+const cors= require('cors')
+const xss= require('xss-clean')
+const rateLimit= require('express-rate-limit')
 const app = express();
+
 
 const connectDB=require('./db/connect')
 //connectDB
+//since our app will be using a reverse proxy we will use trust proxy 
 
+app.set('trust proxy',1)
+
+app.use(rateLimit({
+
+  windowMs:15*60*1000,//15 minutes
+  max:100//limit each IP  to 100  requests per windowMs
+}
+))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 //router
+
 const authRouter= require('./routes/auth')
 const jobRouter=require('./routes/jobs')
 
